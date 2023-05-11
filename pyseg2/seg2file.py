85c98287317518f8440ad96632f71a02c63f3867
mlehujeur <maximilien.lehujeur@gmail.com>
2023-05-11 11:05:03 +0200

    continue packing

--- pyseg2/seg2file.py
+++ pyseg2/seg2file.py
@@ -41,9 +41,7 @@ class FileDescriptorSubBlock:
             buff[2:4], 
             byteorder=self.endian, 
             signed=False)
-        if self.revision_number == 1:
-            pass
-        else:
+        if self.revision_number != 1:
             raise NotImplementedError(self.revision_number)
 
         # ==================================        
@@ -97,7 +95,72 @@ class FileDescriptorSubBlock:
         assert len(self.reserved) == 32 - 14, (len(self.reserved))
 
     def pack(self) -> bytes:
-        raise NotImplementedError
+        buff = bytearray(b"\x00" * 32)
+
+        # =================
+        if self.endian == "little":
+            buff[0:2] = b"\x55\x3a"
+        elif self.endian == "big":
+            buff[0:2] = b"\x3a\x55"
+        else:
+            raise ValueError(self.endian)
+
+        # =================
+        if self.revision_number != 1:
+            raise NotImplementedError(self.revision_number)
+
+        buff[2:4] = int.to_bytes(
+            self.revision_number, length=2,
+            byteorder=self.endian, signed=False)
+
+        # =================
+        assert 4 <= self.size_of_trace_pointer_subblock <= 65532
+        assert not self.size_of_trace_pointer_subblock % 4
+        # WARNING : size must match with the trace pointer subblock
+        buff[4:6] = int.to_bytes(
+            self.size_of_trace_pointer_subblock,
+            length=2,
+            byteorder=self.endian,
+            signed=False)
+
+        # =================
+        assert 1 <= self.number_of_traces <= 16383
+        assert 1 <= self.number_of_traces <= self.size_of_trace_pointer_subblock // 4
+        buff[6:8] = int.to_bytes(
+            self.number_of_traces,
+            length=2,
+            byteorder=self.endian,
+            signed=False)
+
+        # ==================================
+        assert self.string_terminator_length in [1, 2], ValueError(self.string_terminator_length)
+        assert len(self.string_terminator) == self.string_terminator_length
+        buff[8:9] = int.to_bytes(
+            self.string_terminator_length,
+            length=1,
+            byteorder=self.endian,
+            signed=False)
+
+        # ==================================
+        buff[9:9 + self.string_terminator_length] = self.string_terminator
+
+        # ==================================
+        assert len(self.line_terminator) == self.line_terminator_length
+        assert self.line_terminator_length in [0, 1, 2], self.line_terminator_length
+        buff[11:12] = int.to_bytes(
+            self.line_terminator_length,
+            length=1,
+            byteorder=self.endian,
+            signed=False)
+
+        # =================
+        buff[12:12 + self.line_terminator_length] = self.line_terminator
+
+        # =================
+        assert len(self.reserved) == 32 - 14
+        buff[14:] = self.reserved
+
+        return bytes(buff)
 
 
 @dataclass
@@ -132,17 +195,51 @@ class TracePointerSubblock:
         self.trace_pointers = np.frombuffer(buff, dtype="uint32", count=self.number_of_traces)
         assert len(self.trace_pointers) == self.number_of_traces, (len(self.trace_pointers))
 
+    def pack(self) -> bytes:
+        assert 4 <= self.size_of_trace_pointer_subblock <= 65532
+        assert not self.size_of_trace_pointer_subblock % 4
+        assert 1 <= self.number_of_traces <= 16383
+        assert 1 <= self.number_of_traces <= self.size_of_trace_pointer_subblock // 4
+
+        assert len(self.trace_pointers) == self.number_of_traces, (len(self.trace_pointers))
+        assert self.trace_pointers.dtype == np.dtype("uint32")
+
+        buff = bytearray(b"\x00" * self.size_of_trace_pointer_subblock)
+        dtype = {"little": ">i4", "big": "<i4"}[self.endian]
+        buff[:self.number_of_traces*4] = \
+            self.trace_pointers.astype(dtype).tobytes()
+
+        return bytes(buff)
+
 
 @dataclass
 class String:
     """
     Strings as stored in the Free Format Section of the File header and Trace headers
     """
+    endian: str = "little"
     offset: int = 0
     text: str = ""
     string_terminator: bytes = b"\x00"
     _key: str = ""
     _value: Optional[object] = None
+
+    def unpack(self, buff):
+        # offset was already read to find the length of buff
+        # self.offset = int.from_bytes(buff[:2], byteorder=self.endian, signed=False)
+
+        self.text = buff[2:-len(self.string_terminator)].decode('ascii')
+
+        string_terminator = buff[-len(self.string_terminator):]
+        assert string_terminator == self.string_terminator
+
+    def pack(self) -> bytes:
+        assert self.offset == len(self.text) + 2 + len(self.string_terminator)
+        buff = bytearray(b'\x00' * self.offset)
+        buff[:2] = int.to_bytes(self.offset, length=2, byteorder=self.endian, signed=False)
+        buff[2:-len(self.string_terminator)] = self.text.encode('ascii')
+        buff[-len(self.string_terminator):] = self.string_terminator
+        return buff
 
     def extract(self):
         text = self.text.strip()
@@ -204,24 +301,46 @@ class FreeFormatSection:
                 buff[i:i+2],
                 byteorder=self.endian, signed=False)
 
-            stringbuff = buff[i+2:i+offset]
-            text = stringbuff[:-len(self.string_terminator)].decode('ascii')
-            tailer = stringbuff[-len(self.string_terminator):]
-            assert tailer == self.string_terminator
-
             string = String(
                 offset=offset,
-                text=text,
-                string_terminator=tailer)
-            print(string)
+                text="",
+                string_terminator=self.string_terminator)
+            string.unpack(buff[i: i+offset])
+
             if self.strings is None:
                 self.strings = [string]
             else:
                 self.strings.append(string)
 
-            if text.startswith('NOTE'):
+            if string.text.startswith('NOTE'):
+                # NOTE is supposed to be the last keyword if present
                 break
             i += offset
+
+    def sort(self):
+        """keys must be listed in alphabetical order, NOTE must be the last one"""
+        keys = [string.key for string in self.strings]
+
+        note = None
+        if "NOTE" in keys:
+            # extract note from self.strings
+            i = keys.index('NOTE')
+            note = self.strings.pop(i)
+            keys.pop(i)
+
+        # sort by alphabetical order
+        self.strings = [self.strings[j] for j in np.argsort(keys)]
+
+        # put note at the end of the list
+        if note is not None:
+            self.strings.append(note)
+
+    def pack(self) -> bytes:
+        self.sort()
+        buff = b""
+        for string in self.strings:
+            buff += string.pack()
+        return buff
 
 
 @dataclass
@@ -266,6 +385,10 @@ class TraceDescriptorBlock:
             )
         self.free_format_section.load(fid)
 
+    def pack(self) -> bytes:
+        raise NotImplementedError
+
+
 
 @dataclass
 class TraceDataBlock:
@@ -292,7 +415,7 @@ class TraceDataBlock:
             b"\x05": np.dtype('float64'),
             }[self.data_format_code]
 
-        self.number_of_bytes =  self.number_of_samples_in_data_block * {
+        self.number_of_bytes = self.number_of_samples_in_data_block * {
             b"\x01": 2,
             b"\x02": 4,
             b"\x04": 4,
@@ -306,6 +429,8 @@ class TraceDataBlock:
     def unpack(self, buff: bytes):
         self.data = np.frombuffer(buff, dtype=self.dtype, count=self.number_of_samples_in_data_block)
 
+    def pack(self) -> bytes:
+        raise NotImplementedError
 
 @dataclass
 class Seg2Trace:
@@ -316,24 +441,25 @@ class Seg2Trace:
 class Seg2File:
     def __init__(self, filename: str):
 
-        self.file_descriptor_sub_block = FileDescriptorSubBlock()
+        self.file_descriptor_subblock = FileDescriptorSubBlock()
         self.trace_pointer_subblock = TracePointerSubblock()
         self.free_format_section = FreeFormatSection()
         self.seg2traces = []
 
         with open(filename, 'rb') as fid:
-            self.file_descriptor_sub_block.load(fid)
+            self.file_descriptor_subblock.load(fid)
 
-            self.trace_pointer_subblock.set(self.file_descriptor_sub_block)
+            self.trace_pointer_subblock.set(self.file_descriptor_subblock)
             self.trace_pointer_subblock.load(fid)
 
             self.free_format_section.set(
-                self.file_descriptor_sub_block,
+                self.file_descriptor_subblock,
                 self.trace_pointer_subblock)
             self.free_format_section.load(fid)
 
             for n, trace_pointer in enumerate(self.trace_pointer_subblock.trace_pointers):
-                # make sure the cursor is positioned at the beginning of the trace
+                # make sure the cursor is positioned at the beginning
+                # of the trace
                 fid.seek(trace_pointer, 0)
 
                 trace_descriptor_block = TraceDescriptorBlock()
@@ -350,8 +476,28 @@ class Seg2File:
                     trace_data_block)
                 self.seg2traces.append(seg2trace)
 
+    def __str__(self):
+        s = f"# ============ File Descriptor\n"
+        s += f"# ===== File Descriptor\n"
+        s += f"{self.file_descriptor_subblock}\n"
+        s += f"# =====\n"
+        s += f"{self.trace_pointer_subblock}\n"
+        s += f"# ============ Free Format Section\n"
+        s += f"{self.free_format_section}\n"
+        for n, trace in enumerate(self.seg2traces):
+             s += f"# ============ Trace # {n}\n"
+             s += str(trace) + "\n"
+        return s
+
 
 if __name__ == "__main__":
 
     seg2 = Seg2File('./toto.seg2')
+    # print(seg2.file_descriptor_subblock)
+    # print(seg2.file_descriptor_subblock.pack())
+    #
+    # print(seg2.trace_pointer_subblock)
+    # print(seg2.trace_pointer_subblock.pack())
 
+    # print(seg2.free_format_section)
+    # print(seg2.free_format_section.pack())
