d12fd5f2922a046ad7d2849eb01ffd7c0678c2d1
mlehujeur <maximilien.lehujeur@gmail.com>
2023-05-12 13:51:59 +0200

    v++

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ import setuptools
 
 setuptools.setup(
     name='pyseg2',
-    version="1.0",
+    version="1.1",
     packages=setuptools.find_packages(),
     install_requires=['numpy', 'pytest'],
     python_requires=">=3.2",  # because of datetime.timezone, see
-    # scripts=[],
     )
