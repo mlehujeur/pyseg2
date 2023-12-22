67977c773d2df256b3347840df56f706df0f069c
mlehujeur <maximilien.lehujeur@univ-eiffel.fr>
2023-12-22 17:00:55 +0100

    setup++

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ import setuptools
 
 setuptools.setup(
     name='pyseg2',
-    version="1.1",
+    version="1.2",
     packages=setuptools.find_packages(),
-    install_requires=['numpy', 'pytest'],
+    install_requires=['numpy' ],
     python_requires=">=3.2",  # because of datetime.timezone, see
     )
